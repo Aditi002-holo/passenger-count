9ec44a7932c225a66089b98dacff3b3f8e100157
Aditi002-holo <aditideshpande2908@gmail.com>
2022-09-07 13:28:47 +0530

    add save button logic

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-let count = 0;
+
 let countEle = document.getElementById("count");
+let saveEle = document.getElementById("save-el");
+let count = 0;
 
 // defined in onclick -  html
 function increment() {
     count += 1;
     countEle.textContent = count;
-
-    console.log(count);
 }
 
 
+// define save function
+// using textContent instead of innerText
+function save() {
+    let saveCount = count + " - ";
+    saveEle.textContent += saveCount;
+    console.log(count);
+}
